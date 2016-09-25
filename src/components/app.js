622a4d80782d573e217453fbb42114150bf30631
skc-garimella <skc.garimella@gmail.com>
2016-09-24 21:05:23 -0500

    commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
 import CourseList from './course_list';
 import CourseDetail from './course-detail';
 import SearchBar from './search-bar';
